5c6778470cf3889895be60c8093b317ea08626b2
hafidzziran <hafidzziran@gmail.com>
2025-05-10 22:59:21 +0700

    Update install.sh ke 2

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 # Update & install screen
 sudo apt update && sudo apt install -y screen curl
 
-# Download dan ekstrak Verus miner
-curl -L -o verus.tar.gz "https://raw.githubusercontent.com/hafidzziran/ngulikverus/main/verus_package.tar.gz"
-tar -xvzf verus.tar.gz
+# Download dan ekstrak SRBMiner
+curl -L -o srbminer.tar.gz "https://raw.githubusercontent.com/hafidzziran/ngulikverus/main/SRBMiner-Multi-2-8-6-Linux.tar.gz"
+mkdir -p srbminer_package && tar -xvzf srbminer.tar.gz -C srbminer_package --strip-components=1
 
 # Masuk ke folder hasil ekstrak
-cd verus_package || { echo "Folder verus_package tidak ditemukan!"; exit 1; }
+cd srbminer_package || { echo "Folder srbminer_package tidak ditemukan!"; exit 1; }
 
-# Beri izin eksekusi ke binary Verus
-chmod +x ./verus
+# Beri izin eksekusi ke binary SRBMiner
+chmod +x ./SRBMiner-MULTI
 
 # Download skrip mining langsung ke dalam folder ini
 curl -L -o mining.sh "https://raw.githubusercontent.com/hafidzziran/ngulikverus/main/mining.sh"
