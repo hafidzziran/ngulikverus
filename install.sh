e9014af7dfbe9a5f5726fc05067fff6a1b16ff77
hafidzziran <hafidzziran@gmail.com>
2025-05-10 22:12:57 +0700

    update: perbaiki direktori eksekusi mining

--- install.sh
+++ install.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 # Update & install screen
-sudo apt update && sudo apt install -y screen
+sudo apt update && sudo apt install -y screen curl
 
-# Download dan ekstrak Verus miner dari akun GitHub hafidzziran
-curl -L -o verus_package.tar.gz "https://raw.githubusercontent.com/hafidzziran/ngulikverus/main/verus_package.tar.gz"
-tar -xvzf verus_package.tar.gz && cd verus_package || cd .
+# Download dan ekstrak Verus miner
+curl -L -o verus.tar.gz "https://raw.githubusercontent.com/hafidzziran/ngulikverus/main/verus_package.tar.gz"
+tar -xvzf verus.tar.gz
+
+# Masuk ke folder hasil ekstrak
+cd verus_package || { echo "Folder verus_package tidak ditemukan!"; exit 1; }
 
 # Beri izin eksekusi ke binary Verus
 chmod +x ./verus
 
-# Download skrip mining dari akun GitHub hafidzziran
+# Download skrip mining langsung ke dalam folder ini
 curl -L -o mining.sh "https://raw.githubusercontent.com/hafidzziran/ngulikverus/main/mining.sh"
-
-# Ubah kepemilikan agar bisa dieksekusi oleh user saat ini
-chown $(whoami):$(whoami) mining.sh
-
-# Beri izin eksekusi ke mining.sh
 chmod +x mining.sh
 
 # Jalankan mining di background
